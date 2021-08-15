9513dc173eecff1de5d533fd7e3c81da2bc3dbaf
Kenneth <kennethwg819@hotmail.com>
2021-08-15 15:14:57 +0800

    add comments

--- AR Sumikko/ViewController.swift
+++ AR Sumikko/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var animations = [String: CAAnimation]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,21 +23,30 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        sceneView.showsStatistics = false
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        sceneView.autoenablesDefaultLighting = true
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        loadAnimations()
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
+        let configuration = ARImageTrackingConfiguration()
+        
+        if let imageToTrack = ARReferenceImage.referenceImages(inGroupNamed: "Coasters", bundle: Bundle.main){
+            
+            configuration.trackingImages = imageToTrack
+            
+            configuration.maximumNumberOfTrackedImages = 2
+            
+            print("detected images added")
+            
+        }
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -46,30 +57,120 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
+    
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+
+        let rootNode = SCNNode()
+        
+        DispatchQueue.main.async {
+            
+        if let imageAnchor = anchor as? ARImageAnchor {
+            
+            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width
+                , height: imageAnchor.referenceImage.physicalSize.height)
+            
+            plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
+            
+            let planeNode = SCNNode(geometry: plane)
+            
+            planeNode.eulerAngles.x = -.pi/2
+            
+            planeNode.name = "planeNode"
+            
+            rootNode.addChildNode(planeNode)
+            
+            // Add reference image to be identified
+            if imageAnchor.referenceImage.name == "tempura_coaster" {
+                
+                // Add 3D model to be shown
+                guard let sumikkoScene = SCNScene(named: "art.scnassets/tempura_0001_idle.dae") else {fatalError("model not imported")}
+                
+                let sumikkoNode = SCNNode()
+                
+                sumikkoNode.name = "sumikkoNode"
+                
+                for childNode in sumikkoScene.rootNode.childNodes {
+                    
+                    sumikkoNode.addChildNode(childNode)
+                    
+                }
+                
+                sumikkoNode.simdScale = simd_float3(3, 3, 3)
+
+                sumikkoNode.eulerAngles.x = .pi/2
+
+                sumikkoNode.position.z = 0.04
+                            
+                planeNode.addChildNode(sumikkoNode)
+                        
+            }
+                
+            }
+            
+            }
+        return rootNode
     }
-*/
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+    
+    // Pre-load animation into 3D model
+    func loadAnimations() {
+        let sceneURL = Bundle.main.url(forResource: "art.scnassets/tempura_0001_shakehands", withExtension: "dae")
+    
+        let sceneSource = SCNSceneSource(url: sceneURL!, options: nil)
+        
+        if let animationObject = sceneSource?.entryWithIdentifier("tempura_0001_shakehands-1", withClass: CAAnimation.self){
+            
+            animationObject.repeatCount = 1
+            
+            animationObject.fadeInDuration = CGFloat(1)
+
+            animationObject.fadeOutDuration = CGFloat(0.5)
+            
+            animations["shakehands"] = animationObject
+            
+        }
+    }
+        
+    // Action when user touches 3D model
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let location = touches.first!.location(in: sceneView)
+        
+        var hitTestOptions = [SCNHitTestOption: Any]()
+        
+        hitTestOptions[SCNHitTestOption.boundingBoxOnly] = true
+        
+        let hitResults: [SCNHitTestResult] = sceneView.hitTest(location, options: hitTestOptions)
+        
+        // When user touches the 3D model
+        if hitResults.first != nil {
+            // Character shakes hands
+            playAnimation(key: "shakehands")
+        } else {
+            // When user touches again, character stops and shakes hands again 
+            stopAnimation(key: "shakehands")
+        }
+    
+    }
+    
+    func playAnimation(key: String){
+        
+        // Add root node for landing spot of 3D model
+        let rootNode = sceneView.scene.rootNode
+        
+        guard let planeNode = rootNode.childNode(withName: "planeNode", recursively: true) else {fatalError("can't find plane node")}
+        
+        guard let sumikkoNode = planeNode.childNode(withName: "sumikkoNode", recursively: true) else {fatalError("can't find sumikko node")}
+        
+        print(sumikkoNode)
+        
+        sumikkoNode.addAnimation(animations[key]!, forKey: key)
         
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
+    func stopAnimation(key: String){
+        sceneView.scene.rootNode.removeAnimation(forKey: key, blendOutDuration: CGFloat(0.5))
     }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+            
 }
